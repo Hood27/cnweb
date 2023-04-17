54d1a7636f91c6f853960f9d0431a4e780d2c6b0
Hood <94836922+Hood27@users.noreply.github.com>
2023-04-17 23:33:05 +0700

    Add files via upload

---
+++ switch/index.php
@@ -0,0 +1,41 @@
+<<<<<<< HEAD
+<?php
+isset($_GET['id']) ? $modelId = (int)$_GET['id'] : exit('error');
+
+require '../tools/modelList.php';
+require '../tools/jsonCompatible.php';
+
+$modelList = new modelList();
+$jsonCompatible = new jsonCompatible();
+
+$modelList = $modelList->get_list();
+$modelSwitchId = $modelId + 1;
+if (!isset($modelList['models'][$modelSwitchId-1])) $modelSwitchId = 1;
+
+header("Content-type: application/json");
+echo $jsonCompatible->json_encode(array('model' => array(
+    'id' => $modelSwitchId,
+    'name' => $modelList['models'][$modelSwitchId-1],
+    'message' => $modelList['messages'][$modelSwitchId-1]
+)));
+=======
+<?php
+isset($_GET['id']) ? $modelId = (int)$_GET['id'] : exit('error');
+
+require '../tools/modelList.php';
+require '../tools/jsonCompatible.php';
+
+$modelList = new modelList();
+$jsonCompatible = new jsonCompatible();
+
+$modelList = $modelList->get_list();
+$modelSwitchId = $modelId + 1;
+if (!isset($modelList['models'][$modelSwitchId-1])) $modelSwitchId = 1;
+
+header("Content-type: application/json");
+echo $jsonCompatible->json_encode(array('model' => array(
+    'id' => $modelSwitchId,
+    'name' => $modelList['models'][$modelSwitchId-1],
+    'message' => $modelList['messages'][$modelSwitchId-1]
+)));
+>>>>>>> 34135c2136ed37613d07faf1d7b0a3b5e80d5617
